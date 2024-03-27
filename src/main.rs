e5a9a6fe3c6d9084c09617382cb4ca43924d38fe
Edgar Christian <56579802+edgarcnp@users.noreply.github.com>
2024-03-27 15:14:01 +0700

    Multi-Threading first implementation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use std::fs;
+use std::time::Duration;
+use std::{fs, thread};
 use std::net::{TcpListener, TcpStream};
 use std::io::prelude::*;
+use threadpool::ThreadPool;
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer: [u8; 1024] = [0; 1024];
@@ -9,9 +11,13 @@ fn handle_connection(mut stream: TcpStream) {
     // println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 
     let get: &[u8; 16] = b"GET / HTTP/1.1\r\n";
+    let sleep: &[u8; 21] = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = 
         if buffer.starts_with(get) {
+            ("HTTP/1.1 200 OK", "index.html")
+        } else if buffer.starts_with(sleep) {
+            thread::sleep(Duration::from_secs(5));
             ("HTTP/1.1 200 OK", "index.html")
         } else {
             ("HTTP/1.1 404 NOT FOUND", "404.html")
@@ -28,10 +34,13 @@ fn handle_connection(mut stream: TcpStream) {
 
 fn main() {
     let listener: TcpListener = TcpListener::bind("127.0.0.1:7878").unwrap();
-
+    let pool: ThreadPool = ThreadPool::new(4);
+    
     for stream in listener.incoming() {
         let stream: TcpStream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
